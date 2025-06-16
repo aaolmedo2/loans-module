aa4aa88d025db42f7dc574b5a80703dc169d7f3f
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-06-16 16:52:11 -0500

    Add a relation of Participe
    Relación de participe para que funcione y se levante el servidor.

--- src/main/java/com/banquito/core/prestamos/enums/EsquemaAmortizacionEnum.java
+++ src/main/java/com/banquito/core/prestamos/enums/EsquemaAmortizacionEnum.java
@@ -1,17 +1,17 @@
 package com.banquito.core.prestamos.enums;
 
-public enum NombreEsquemaAmortizacionEnum {
+public enum EsquemaAmortizacionEnum {
     FRANCES("FRANCES"),
     AMERICANO("AMERICANO"),
     ALEMAN("ALEMAN");
 
     private final String valor;
 
-    NombreEsquemaAmortizacionEnum(String valor) {
+    EsquemaAmortizacionEnum(String valor) {
         this.valor = valor;
     }
 
     public String getValor() {
         return valor;
     }
-} 
+}
