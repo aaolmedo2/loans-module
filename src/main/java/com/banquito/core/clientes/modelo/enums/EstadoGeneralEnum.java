a4d34ba93032a160c203ed478f8d9e765c96f32b
Freddy Leonel Pachacama <flpachacama@espe.edu.ec>
2025-06-16 16:45:08 -0500

    Fixed bugs in EstadoEnums

--- src/main/java/com/banquito/core/clientes/modelo/enums/EstadoGeneralEnum.java
+++ src/main/java/com/banquito/core/clientes/modelo/enums/EstadoGeneralEnum.java
@@ -1,12 +1,12 @@
 package com.banquito.core.clientes.modelo.enums;
 
-public enum EstadoAccionistaEnum {
+public enum EstadoGeneralEnum {
     ACTIVO("ACTIVO"),
     INACTIVO("INACTIVO");
 
     private final String valor;
 
-    EstadoAccionistaEnum(String valor) {
+    EstadoGeneralEnum(String valor) {
         this.valor = valor;
     }
 
